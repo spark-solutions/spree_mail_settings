8df7d7d234c7e6c282bbb8831c9be36676b60f21
Mafi <michal.markiewicz@sparksolutions.co>
2016-03-10 15:09:17 +0100

    Always use Spree::Config[:mails_from] if present

--- lib/spree/core/mail_interceptor.rb
+++ lib/spree/core/mail_interceptor.rb
@@ -6,7 +6,7 @@ module Spree
   module Core
     class MailInterceptor
       def self.delivering_email(message)
-        message.from ||= Config[:mails_from] if Config[:mails_from].present?
+        message.from = Config[:mails_from] if Config[:mails_from].present?
 
         if Config[:intercept_email].present?
           message.subject = "#{message.to} #{message.subject}"
